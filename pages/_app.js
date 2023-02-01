4bee71086dde2fb95f70e3997648b5be348a9278
Aras Örs <arasinthehell@gmail.com>
2023-02-01 11:25:06 +0300

    start

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import '../styles/tailwind.css'
+import { ThemeProvider } from "@material-tailwind/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+      <ThemeProvider value={}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+  )
 }
 
 export default MyApp
