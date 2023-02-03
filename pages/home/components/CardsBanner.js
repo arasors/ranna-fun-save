3a4f795bae1787bae6cfe5bcc76665465971dbf2
Aras Örs <arasinthehell@gmail.com>
2023-02-03 18:40:32 +0300

    reponsive ok

--- pages/home/components/CardsBanner.js
+++ pages/home/components/CardsBanner.js
@@ -10,14 +10,14 @@ const CardsBanner = () => {
 
     return(
         <div className={classNames({
-            "w-full py-8": true,
+            "w-full py-8  md:px-0 px-4": true,
             "bg-pr-pattern relative overflow-hidden": true
         })}>
             <div className={classNames({
-                "absolute top-12 left-1/2 h-full w-[48%]": true,
+                "absolute top-12 left-1/2 h-full w-[48%] md:block hidden": true,
                 "bg-enthusiastic-joyful bg-right bg-contain bg-no-repeat": true
             })}></div>
-            <div className="container mx-auto py-8 w-full text-white flex-row">
+            <div className="container mx-auto py-8 w-full text-white md:flex-row">
                 <div className={"flex-1"}>
                     <h3 className={"text-3xl font-bold mt-14 pt-14"}>{t("credit-card-banner-title")}</h3>
                     <p className="py-8">{t("credit-card-banner-text")}</p>
@@ -25,7 +25,7 @@ const CardsBanner = () => {
 
 
 
-                    <div className={"flex-row gap-x-2"}>
+                    <div className={"md:flex-row gap-2 items-center justify-center w-full"}>
 
                         {[
                             {
--- pages/home/components/HowWork.js
+++ pages/home/components/HowWork.js
@@ -9,12 +9,12 @@ const HowWork = () => {
 
     return(
         <div className={classNames({
-            "w-full py-8": true,
+            "w-full py-12": true,
             "bg-pr-pattern2 relative overflow-hidden": true
         })}>
-            <div className="container max-w-screen-xl mx-auto py-8 w-full text-white flex-row">
+            <div className="container max-w-screen-xl mx-auto py-8 w-full text-white md:flex-row px-4 md:px-0">
                 <div className={"flex-1"}>
-                    <h3 className={"text-3xl font-bold mt-14 pt-14"}>{t("how-does-it-work-summary-title")}</h3>
+                    <h3 className={"text-3xl font-bold"}>{t("how-does-it-work-summary-title")}</h3>
                     <p className="py-8">{t("how-does-it-work-summary-text")}</p>
                 </div>
                 <div className={"flex-1"}>
